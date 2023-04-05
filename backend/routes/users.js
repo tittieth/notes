82e76c4eb706abf834f8094932ea031fe75ba544
tittieth <114925706+tittieth@users.noreply.github.com>
2023-04-05 14:09:33 +0200

    login router working

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,7 +4,6 @@ const connection = require("../conn");
 
 /* GET users listing. */
 router.get("/", (req, res) => {
-
   connection.connect((err) => {
     if (err) {
       console.log("err", err);
@@ -34,9 +33,9 @@ router.post("/add", (req, res) => {
 
     let sql = "INSERT INTO users (userName, userPassword, userEmail) VALUES (?, ?, ?)";
     let values = [
-      encodeURI(newUser.newUserName), 
-      encodeURI(newUser.newUserPassword), 
-      encodeURI(newUser.newUserEmail)
+      encodeURI(newUser.newUserName),
+      encodeURI(newUser.newUserPassword),
+      encodeURI(newUser.newUserEmail),
     ];
 
     connection.query(sql, values, (err, data) => {
@@ -47,6 +46,38 @@ router.post("/add", (req, res) => {
       console.log("data från query", data);
       res.json(data);
     });
+  });
+});
+
+/* Login user */
+
+router.post("/login", (req, res) => {
+  const userName = req.body.userName;
+  const password = req.body.userPassword;
+
+  connection.connect((err) => {
+    if (err) {
+      console.log("err", err);
+    }
+
+    if (userName && password) {
+      let encodedUserName = encodeURI(userName);
+      let sql = "SELECT * FROM users WHERE userName = ?";
+
+      connection.query(sql, encodedUserName, (err, data) => {
+        if (data.length > 0) {
+          if (data[0].userPassword === password) {
+            res.json(data);
+          } else {
+            res.send("Fel lösenord");
+          }
+        } else {
+          res.send("Felaktigt användarnamn");
+        }
+      });
+    } else {
+      res.send("Fyll i användarnamn och lösenord");
+    }
   });
 });
 
