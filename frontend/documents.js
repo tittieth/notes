a0912eefe250a5832317167641785de25b199cce
tittieth <114925706+tittieth@users.noreply.github.com>
2023-04-08 21:25:49 +0200

    :lipstick: styling for documents

--- frontend/documents.js
+++ frontend/documents.js
@@ -8,16 +8,22 @@ export default function printDocuments() {
         console.log(documents);
 
         let documentsWrapper = document.createElement("div");
+        documentsWrapper.classList.add("documents-wrapper");
+
         let documentsList = document.createElement("ul");
         documentsList.classList.add("blogPosts");
         documentsList.innerHTML = "";
+
+        let readMoreBtn = document.createElement("button");
+        readMoreBtn.innerText = "Läs mer"
+
         documentsWrapper.appendChild(documentsList);
 
         documents.map(post => {
             let li = document.createElement("li");
             li.id = post.documentTitle;
-            li.innerHTML = `<h1>${post.documentTitle}</h1><p>${post.documentContent}</p>`;
-            documentsList.appendChild(li);
+            li.innerHTML = `<h2>${post.documentTitle}</h2><p>${post.documentContent}</p><button>Läs mer</button>`;
+            documentsList.append(li);
     })
 
     app.innerHTML = "";
